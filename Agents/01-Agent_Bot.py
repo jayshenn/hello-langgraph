2ae2338e4b036a4ac7ed75db9d978080f6158e72
jayshenn <jiuyang1215@gmail.com>
2025-09-22 01:28:06 +0800

    Agent I

---
+++ Agents/01-Agent_Bot.py
@@ -0,0 +1,33 @@
+from typing import TypedDict, List
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("DEEPSEEK_API_KEY")
+base_url = os.getenv("DEEPSEEK_BASE_URL")
+model = os.getenv("DEEPSEEK_MODEL")
+
+llm = ChatOpenAI(model=model,base_url=base_url,api_key=api_key)
+
+class AgentState(TypedDict):
+    messages: List[HumanMessage]
+
+def process(state: AgentState) -> AgentState:
+    response = llm.invoke(state["messages"])
+    print(f"\nAI: {response.content}")
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process",process)
+graph.add_edge(START,"process")
+graph.add_edge("process",END)
+agent = graph.compile()
+
+user_input = input("Enter: ")
+while user_input != "exit":
+    agent.invoke({"messages":[HumanMessage(content=user_input)]})
+    user_input = input("Enter: ")
